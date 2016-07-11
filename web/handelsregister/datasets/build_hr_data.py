490d217af6379ceaf5a41354c1fa67519b3c4e3d
Stephan Preeker <stephan@preeker.net>
2016-07-11 16:06:34 +0200

    start working on import script

---
+++ web/handelsregister/datasets/build_hr_data.py
@@ -0,0 +1,13 @@
+"""
+From the original dump
+
+fill the stelselpedia dumps
+"""
+
+
+def fill_stelselpedia():
+    """
+    For go through all tables and fill stelselpedia tables
+    """
+
+    print('not yet implemented')
---
+++ web/handelsregister/handelsregister/management/commands/run_import.py
@@ -0,0 +1,39 @@
+# import sys
+
+from django.core.management import BaseCommand
+
+from datasets import build_hr_data
+
+import logging
+
+log = logging.getLogger(__name__)
+
+
+class Command(BaseCommand):
+    """
+    Import HR data from handel regster dump
+
+    clear data using:
+
+    - manage.py migrate bbga_data zero
+
+    apply new/updated migrations
+
+    - manage.py migrate bbga_data
+
+    """
+
+    # def add_arguments(self, parser):
+
+    #    parser.add_argument(
+    #        'csv_source',
+    #        nargs=1,
+    #        type=str,
+    #        help='CSV bron bestand')
+
+    def handle(self, *args, **options):
+        """
+        validate and execute import task
+        """
+
+        build_hr_data.fill_stelselpedia()
