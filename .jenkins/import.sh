4c6449e1cb606ac8fa6ed7c5a5a42b9e150790f6
Stephan Preeker <stephan@preeker.net>
2016-07-14 14:24:03 +0200

    added import.sh scrit (todo test)

--- .jenkins/import.sh
+++ .jenkins/import.sh
@@ -6,7 +6,7 @@ set -u
 DIR="$(dirname $0)"
 
 dc() {
-	docker-compose -f ${DIR}/docker-compose.yml $*
+	docker-compose -f ${DIR}/docker-compose.yml $*;
 }
 
 trap 'dc kill ; dc rm -f' EXIT
@@ -15,6 +15,15 @@ rm -rf ${DIR}/backups
 mkdir -p ${DIR}/backups
 
 dc build
-dc run --rm importer
-dc run --rm db-backup
+# import new mks dump data in database
+dc run --rm importer_mks
+# create the hr_data
+dc up importer_1 importer_2
+# wait until all building is done
+docker wait jenkins_importer_1_1 jenkins_importer_2_1
 
+# run the backup shizzle
+dc run --rm db-backup
+#dc run --rm el-backup
+
+# You are awesome and done! <3
