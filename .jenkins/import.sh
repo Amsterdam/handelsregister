d12d289801694a5f202491f4924624ba7ba0c767
Stephan Preeker <stephan@preeker.net>
2016-08-18 15:44:06 +0200

    correctly check import errors

--- .jenkins/import.sh
+++ .jenkins/import.sh
@@ -20,14 +20,14 @@ dc run --rm importer_mks
 # create the hr_data
 dc up importer_1  importer_2 importer_3
 # wait until all building is done
-import_error=`docker wait jenkins_importer_1_1 jenkins_importer_2_1 jenkins_importer_3_1`
+import_status=`docker wait jenkins_importer_1_1 jenkins_importer_2_1 jenkins_importer_3_1`
 
 # count the errors.
-import_error=`echo $import_error | grep -o "1" | wc -l`
+import_error=`echo $import_status | grep -o "1" | wc -l`
 
 echo $import_error
 
-if [ $import_error = 1 ]
+if [ $import_error > 0 ]
 then
     echo 'Import Error!! You SUCK!'
     exit -1
