2b57fe591b3e5e1a5300a5e885a3c4c9d41ab65e
Yigal Duppen <yigal@publysher.nl>
2016-08-23 10:40:12 +0200

    TG-9 HR Import: read MKS files

--- web/handelsregister/loaddumps.sh
+++ web/handelsregister/loaddumps.sh
@@ -20,6 +20,8 @@ for sql in *.sql; do
     grep -v OWNER $sql | grep -v search_path | grep -v REVOKE |  \
     grep -v GRANT | \
     grep -v TRIGGER | \
+    grep -v DROP | \
     sed 's/^.*geometry(Point.*$/    geopunt GEOMETRY(Point,28992)/' | \
+    sed 's/igp_sw44z0001_cmg_owner\.//' | \
     psql -v ON_ERROR_STOP=1 -d handelsregister -h ${DATABASE_PORT_5432_TCP_ADDR} -U handelsregister
 done
