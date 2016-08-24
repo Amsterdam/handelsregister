6949fb2103b6cf45e18a64cde5c86e2ead82ddd9
Yigal Duppen <yigal@publysher.nl>
2016-08-24 13:24:53 +0200

    Fix import script

--- .jenkins/import.sh
+++ .jenkins/import.sh
@@ -6,7 +6,7 @@ set -u
 DIR="$(dirname $0)"
 
 dc() {
-	docker-compose -f ${DIR}/docker-compose.yml $*;
+	docker-compose -p hr_import -f ${DIR}/docker-compose.yml $*;
 }
 
 trap 'dc kill ; dc rm -f' EXIT
@@ -15,26 +15,9 @@ rm -rf ${DIR}/backups
 mkdir -p ${DIR}/backups
 
 dc build --pull
-# import new mks dump data in database
 dc run --rm importer_mks
 # create the hr_data
-dc up importer
-# wait until all building is done
-import_status=`docker wait jenkins_importer_1`
-
-# count the errors.
-import_error=`echo $import_status | grep -o "1" | wc -l`
-
-echo $import_error
-
-if [ $import_error > 0 ]
-then
-    echo 'Import Error!! You SUCK!'
-    exit -1
-fi
+dc run --rm importer
 
 # run the backup shizzle
 dc run --rm db-backup
-#dc run --rm el-backup
-
-echo 'You are awesome and done! <3'
