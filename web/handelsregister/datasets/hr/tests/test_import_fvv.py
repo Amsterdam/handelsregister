1d4cf1d8bbcbec5910fe3b9f25861e5f1f9d821e
Jan van Hellemond <jan@frontlab.nl>
2016-09-13 15:20:01 +0200

    More (better) tests for Functievervulling related fields.

--- web/handelsregister/datasets/hr/tests/test_import_fvv.py
+++ web/handelsregister/datasets/hr/tests/test_import_fvv.py
@@ -18,6 +18,32 @@ class ImportFunctievervullingTest(TestCase):
         return models.Functievervulling.objects.get(pk=fvv.pk)
 
     def test_import_typical_example(self):
+        prs1 = kvk.KvkPersoon.objects.create(
+            prsid=Decimal('200000000000000000'),
+            prshibver=Decimal('100000000000000000'),
+				    faillissement='Nee',
+				    naam='Testpersoon 1 B.V.',
+				    persoonsrechtsvorm='BeslotenVennootschap',
+				    rsin='000000001',
+				    typering='rechtspersoon',
+				    uitgebreiderechtsvorm='BeslotenVennootschap',
+				    verkortenaam='Testpersoon 1 Verkort B.V.',
+				    volledigenaam='Testpersoon 1 Volledig B.V.',
+				    rol='EIGENAAR'
+        )
+        prs2 = kvk.KvkPersoon.objects.create(
+            prsid=Decimal('300000000000000000'),
+            prshibver=Decimal('100000000000000000'),
+				    faillissement='Nee',
+				    naam='Testpersoon 2 B.V.',
+				    persoonsrechtsvorm='BeslotenVennootschap',
+				    rsin='000000001',
+				    typering='rechtspersoon',
+				    uitgebreiderechtsvorm='BeslotenVennootschap',
+				    verkortenaam='Testpersoon 2 Verkort B.V.',
+				    volledigenaam='Testpersoon 2 Volledig B.V.',
+				    rol='EIGENAAR'
+        )
         fvv = kvk.KvkFunctievervulling.objects.create(
             ashid=Decimal('100000000000000000'),
             functie="GrandPoobah",
@@ -32,5 +58,33 @@ class ImportFunctievervullingTest(TestCase):
         self.assertEqual('100000000000000000', functievervulling.id)
         self.assertEqual('GrandPoobah', functievervulling.functietitel)
         self.assertEqual('OnbeperktBevoegd', functievervulling.soortbevoegdheid)
-        self.assertEqual(Decimal('300000000000000000'), functievervulling.heeft_aansprakelijke_id)
-        self.assertEqual(Decimal('200000000000000000'), functievervulling.is_aansprakelijke_id)
+        self.assertEqual(Decimal('300000000000000000'), functievervulling.heeft_aansprakelijke.id)
+        self.assertEqual(Decimal('200000000000000000'), functievervulling.is_aansprakelijke.id)
+
+    def test_import_missing_related(self):
+        prs1 = kvk.KvkPersoon.objects.create(
+            prsid=Decimal('200000000000000000'),
+            prshibver=Decimal('100000000000000000'),
+				    faillissement='Nee',
+				    naam='Testpersoon 1 B.V.',
+				    persoonsrechtsvorm='BeslotenVennootschap',
+				    rsin='000000001',
+				    typering='rechtspersoon',
+				    uitgebreiderechtsvorm='BeslotenVennootschap',
+				    verkortenaam='Testpersoon Verkort B.V.',
+				    volledigenaam='Testpersoon Volledig B.V.',
+				    rol='EIGENAAR'
+        )
+        fvv = kvk.KvkFunctievervulling.objects.create(
+            ashid=Decimal('100000000000000000'),
+            functie="GrandPoobah",
+            prsidi=Decimal('200000000000000000'),
+            prsidh=Decimal('300000000000000000'),
+            soort="OnbeperktBevoegd",
+            prsashhibver=Decimal('100000000000000000'),
+        )
+        functievervulling = self._convert(fvv)
+
+        self.assertIsNotNone(functievervulling)
+        self.assertEqual(Decimal('200000000000000000'), functievervulling.is_aansprakelijke.id)
+        self.assertRaises(models.Persoon.DoesNotExist, lambda: functievervulling.heeft_aansprakelijke)
