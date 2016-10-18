e92115d301a289897ae752f9dc707f51c1d7961d
Stephan Preeker <stephan@preeker.net>
2016-10-18 22:32:11 +0200

    check the right tables..

--- web/handelsregister/handelsregister/management/commands/run_import.py
+++ web/handelsregister/handelsregister/management/commands/run_import.py
@@ -73,5 +73,6 @@ class Command(BaseCommand):
             build_hr_data.fill_stelselpedia()
             # now update mks locations with bag locations
             build_hr_data.fill_location_with_bag()
-            log.info('hr_geovestigingen %s', models.GeoVestigingen.objects.count())
-            assert models.GeoVestigingen.objects.count() > 200.000
+            log.info('hr_geovestigingen %s', models.Locatie.objects.count())
+            assert models.GeoVestigingen.objects.count() == 0
+            assert models.Locatie.objects.count() > 200000
