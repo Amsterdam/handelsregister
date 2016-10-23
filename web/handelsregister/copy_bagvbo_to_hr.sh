d55fdb35a4e93dd1ab87b43d1b6d68e3f38792a8
Stephan Preeker <stephan@preeker.net>
2016-10-23 14:22:51 +0200

    add missing sh script

---
+++ web/handelsregister/copy_bagvbo_to_hr.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -u
+set -e
+
+chmod 0600 .pgpass
+
+echo copying bag vbo/numid geo -> hr_geovbo tabel
+echo database:5432:handelsregister:handelsregister:insecure >> ~/.pgpass
+echo database:5432:atlas:handelsregister:insecure >> ~/.pgpass
+chmod 0600 ~/.pgpass
+
+echo copying bag vbo geo -> hr_geovbo tabel
+
+psql -U handelsregister -h database atlas -c \
+        '\copy (SELECT v.id, v.landelijk_id, n.landelijk_id, geometrie from bag_verblijfsobject v, bag_nummeraanduiding n WHERE n.verblijfsobject_id = v.id AND n.hoofdadres) TO STDOUT' \
+        | psql -U handelsregister -h database handelsregister -c \
+                '\copy hr_geovbo (id, bag_vbid, bag_numid, geometrie) FROM STDIN'
