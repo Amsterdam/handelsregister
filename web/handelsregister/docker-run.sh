95deeea78f415904b70fa14b8b56d1d8cb56d7d5
Jeroen van 't Hert <jeroen@yottabyte.io>
2016-12-12 16:23:40 +0100

    Not needed for HR

--- web/handelsregister/docker-run.sh
+++ web/handelsregister/docker-run.sh
@@ -7,8 +7,5 @@ source docker-wait.sh
 
 source docker-migrate.sh || echo "Could not migrate, ignoring"
 
-echo Collecting static files
-yes yes | python manage.py collectstatic || echo "Could not loac static content, ignoring"
-
 # run uwsgi
 exec uwsgi --ini /app/uwsgi.ini
