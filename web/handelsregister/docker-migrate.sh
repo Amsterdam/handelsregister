b5db75b85fea2b46127780c437cdca18c041a1b0
Stephan Preeker <stephan@preeker.net>
2016-12-18 22:00:02 +0100

    reset geoviews on deploy, improve error message

--- web/handelsregister/docker-migrate.sh
+++ web/handelsregister/docker-migrate.sh
@@ -4,4 +4,5 @@ set -u   # crash on missing env variables
 set -e   # stop on any error
 
 source docker-wait.sh
+yes yes | python manage.py geo_views zero --noinput
 yes yes | python manage.py migrate --noinput
