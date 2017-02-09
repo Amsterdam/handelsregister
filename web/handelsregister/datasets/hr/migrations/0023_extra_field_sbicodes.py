58f093924c71757a385f2884125983ac2fda8c50
Marten Hoekstra <marten@dolhuis.nl>
2017-02-09 13:35:55 +0100

    TG-665 backend: fixt conflict in migratie

--- web/handelsregister/datasets/hr/migrations/0023_extra_field_sbicodes.py
+++ web/handelsregister/datasets/hr/migrations/0023_extra_field_sbicodes.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('hr', '0020_extra_sbicodes'),
+        ('hr', '0022_delete_geovbo'),
     ]
 
     operations = [
