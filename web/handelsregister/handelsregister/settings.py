e3cd4f928d68b20bdbc25991425cc80443427cfa
Bart Heupers <bart.heupers@gmail.com>
2017-11-14 16:52:33 +0100

    Fix JWKS. Set complete test sign and verify JWKS in settings

--- web/handelsregister/handelsregister/settings.py
+++ web/handelsregister/handelsregister/settings.py
@@ -315,7 +315,7 @@ LOGSTASH_HOST = os.getenv('LOGSTASH_HOST', '127.0.0.1')
 LOGSTASH_PORT = int(os.getenv('LOGSTASH_GELF_UDP_PORT', 12201))
 
 # The following JWKS data was obtained in the authz project :  jwkgen -create -alg ES256
-# This is a test public key def. The private key is defined mixins.py for testing.
+# This is a test public/private key def and added for testing.
 JWKS_TEST_KEY = """
     {
         "keys": [
@@ -329,6 +329,7 @@ JWKS_TEST_KEY = """
                 "crv": "P-256",
                 "x": "6r8PYwqfZbq_QzoMA4tzJJsYUIIXdeyPA27qTgEJCDw=",
                 "y": "Cf2clfAfFuuCB06NMfIat9ultkMyrMQO9Hd2H7O9ZVE=",
+                "d": "N1vu0UQUp0vLfaNeM0EDbl4quvvL6m_ltjoAXXzkI3U="
             }
         ]
     }
--- web/handelsregister/handelsregister/tests/authorization.py
+++ web/handelsregister/handelsregister/tests/authorization.py
@@ -1,7 +1,6 @@
 import time
 import jwt
 
-from django.conf import settings
 from authorization_django.config import settings as middleware_settings
 import authorization_levels
 
@@ -27,25 +26,7 @@ class AuthorizationSetup(object):
             HTTP_AUTHORIZATION='Bearer {}'.format(self.token_scope_hr))
 
         """
-        # VERY NEW STYLE AUTH
-        settings.DATAPUNT_AUTHZ['JWKS'] = """
-            {
-                "keys": [
-                    {
-                        "kty": "EC",
-                        "key_ops": [
-                            "verify",
-                            "sign"
-                        ],
-                        "kid": "2aedafba-8170-4064-b704-ce92b7c89cc6",
-                        "crv": "P-256",
-                        "x": "6r8PYwqfZbq_QzoMA4tzJJsYUIIXdeyPA27qTgEJCDw=",
-                        "y": "Cf2clfAfFuuCB06NMfIat9ultkMyrMQO9Hd2H7O9ZVE=",
-                        "d": "N1vu0UQUp0vLfaNeM0EDbl4quvvL6m_ltjoAXXzkI3U="
-                    }
-                ]
-            }
-        """
+        # VERY NEW STYLE AUTH. JWKS public/private keys are defined in settings
         jwks = middleware_settings()['JWKS'].signers
 
         assert len(jwks) > 0
