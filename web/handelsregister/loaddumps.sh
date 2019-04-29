cc171ab0a064ac1ab51a11cd4863c7dc050cf4aa
Bart Heupers <bart.heupers@gmail.com>
2019-04-29 12:53:28 +0200

    Fix import by excluding DDL statements in SQL more selectively

--- web/handelsregister/loaddumps.sh
+++ web/handelsregister/loaddumps.sh
@@ -21,13 +21,13 @@ cd /app/unzipped/
 
 for sql in *.sql; do
     grep -v OWNER $sql | grep -v search_path | grep -v REVOKE |  \
-    grep -v GRANT | \
-    grep -v TRIGGER | \
-    grep -v DROP | \
+    grep -v ^GRANT | \
+    grep -v "CREATE TRIGGER" | \
+    grep -v ^DROP | \
     grep -v "CREATE INDEX" | \
     grep -v "ADD CONSTRAINT" | \
     grep -v "ALTER TABLE" | \
-    grep -v "PRIMARY" | \
+    grep -v "PRIMARY KEY (" | \
     sed 's/^.*geometry(Point.*$/    geopunt GEOMETRY(Point,28992)/' | \
     sed 's/igp_sw44z0001_cmg_owner\.//' | \
     psql -v ON_ERROR_STOP=1 -d handelsregister -h database -U handelsregister
--- web/handelsregister/loaddumps_local.sh
+++ web/handelsregister/loaddumps_local.sh
@@ -23,13 +23,13 @@ cd unzipped/
 
 for sql in *.sql; do
     grep -v OWNER $sql | grep -v search_path | grep -v REVOKE |  \
-    grep -v GRANT | \
-    grep -v TRIGGER | \
-    grep -v DROP | \
+    grep -v ^GRANT | \
+    grep -v "CREATE TRIGGER" | \
+    grep -v ^DROP | \
     grep -v "CREATE INDEX" | \
     grep -v "ADD CONSTRAINT" | \
     grep -v "ALTER TABLE" | \
-    grep -v "PRIMARY" | \
+    grep -v "PRIMARY KEY (" | \
     sed 's/^.*geometry(Point.*$/    geopunt GEOMETRY(Point,28992)/' | \
     sed 's/igp_sw44z0001_cmg_owner\.//' | \
     psql -v ON_ERROR_STOP=1 -d handelsregister -h ${DATABASE_PORT_5432_TCP_ADDR} -p  ${DATABASE_PORT_5432_TCP_PORT} -U handelsregister
