9dd874272a3f6bdc9e3bc0a9fcbcb5000ab7ba82
Bart Heupers <bart.heupers@gmail.com>
2020-03-10 14:40:44 +0100

    Set sentry environment to "handelsregister"

--- web/handelsregister/handelsregister/settings.py
+++ web/handelsregister/handelsregister/settings.py
@@ -419,5 +419,6 @@ SENTRY_DSN = os.getenv('SENTRY_DSN')
 if SENTRY_DSN:
     sentry_sdk.init(
         dsn=SENTRY_DSN,
+        environment="handelsregister",
         integrations=[DjangoIntegration()]
     )
