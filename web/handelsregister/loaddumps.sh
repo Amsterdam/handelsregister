07b95b898392d7e25c4263a3b6a9927eda0f0940
Roel de Vries <roel.de.vries@amsterdam.nl>
2023-04-20 10:57:47 +0200

    Temp filter mac id from dumps

--- web/handelsregister/loaddumps.sh
+++ web/handelsregister/loaddumps.sh
@@ -20,6 +20,7 @@ cd /app/unzipped/
 # Load all sql files
 # Ignore lines which match certain patterns (grep)
 # or replace values with '' (sed)
+# TEMPORARY VES filter on mac id 100000000012419559
 for sql in *.sql; do
     grep -v OWNER $sql | grep -v search_path | grep -v REVOKE |  \
     grep -v ^GRANT | \
@@ -29,6 +30,28 @@ for sql in *.sql; do
     grep -v "ADD CONSTRAINT" | \
     grep -v "ALTER TABLE" | \
     grep -v "PRIMARY KEY (" | \
+    grep -v 100000000012419606 | \
+    grep -v 100000000012419529 | \
+    grep -v 100000000012419505 | \
+    grep -v 100000000012419495 | \
+    grep -v 100000000012419575 | \
+    grep -v 100000000012419612 | \
+    grep -v 100000000012419586 | \
+    grep -v 100000000012419477 | \
+    grep -v 100000000012419523 | \
+    grep -v 100000000012419559 | \
+    grep -v 100000000012419483 | \
+    grep -v 100000000012419565 | \
+    grep -v 100000000012419592 | \
+    grep -v 100000000012419553 | \
+    grep -v 100000000012419535 | \
+    grep -v 100000000012419547 | \
+    grep -v 100000000012419511 | \
+    grep -v 100000000012419517 | \
+    grep -v 100000000012419471 | \
+    grep -v 100000000012419489 | \
+    grep -v 100000000012419541 | \
+    grep -v 100000000012419465 | \
     sed 's/^.*geometry(Point.*$/    geopunt GEOMETRY(Point,28992)/' | \
     sed -r 's/igp_[a-zA-Z0-9_]+_cmg_owner\.//' | \
     psql -v ON_ERROR_STOP=1 -d handelsregister -h database -U handelsregister
